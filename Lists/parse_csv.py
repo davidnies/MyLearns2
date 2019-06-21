6056c2c06e7ba805d71407023c74605384cbbed3
David Nies <davidjnies@outlook.com>
2019-06-21 18:57:35 -0500

    completed several videos of training

---
+++ Lists/parse_csv.py
@@ -0,0 +1,28 @@
+import csv
+
+# 'r' = read
+with open('names.csv', 'r') as csv_file:
+
+    csv_reader = csv.reader(csv_file)
+    #  print(csv_reader)
+    next(csv_reader)   # skips header row.
+
+    for line in csv_reader:
+      #  print(line)
+        print(line[2])
+
+
+'''
+    csv_reader = csv.DictReader(csv_file)
+
+    with open('new_names.csv', 'w') as new_file:
+        fieldnames = ['first_name', 'last_name']
+
+        csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter='\t')
+
+        csv_writer.writeheader()
+
+        for line in csv_reader:
+            del line['email']
+            csv_writer.writerow(line)
+'''
