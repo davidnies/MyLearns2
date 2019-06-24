84825bb1f831a26ff6889b2198c8375aad6d4669
David <djnies@hotmail.com>
2019-06-23 21:31:58 -0500

    Finished video

--- Lists/parse_csv.py
+++ Lists/parse_csv.py
@@ -2,23 +2,24 @@
 
 import csv
 
-# 'r' = read
-with open('names.csv', 'r') as csv_file:
 
-    csv_reader = csv.reader(csv_file)
-    #  print(csv_reader)
-    next(csv_reader)   # skips header row.
+with open('names.csv', 'r') as csv_file:   # 'r' = read
+    # csv_reader = csv.reader(csv_file)
+    #
+    #
+    # with open('new_names.csv', 'w') as new_file:
+    #     csv_writer = csv.writer(new_file, delimiter='\t')
+    #
+    #     for line in csv_reader:
+    #         csv_writer.writerow(line)
 
-    for line in csv_reader:
-      #  print(line)
-        print(line[2])
-
-
-'''
     csv_reader = csv.DictReader(csv_file)
+    # for line in csv_reader:
+    #     print(line['email'])   #'email' - is the key in the Dictionary
+
 
     with open('new_names.csv', 'w') as new_file:
-        fieldnames = ['first_name', 'last_name']
+        fieldnames = ['first_name', 'last_name', 'email']
 
         csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter='\t')
 
@@ -27,4 +28,4 @@ with open('names.csv', 'r') as csv_file:
         for line in csv_reader:
             del line['email']
             csv_writer.writerow(line)
-'''
+    #
