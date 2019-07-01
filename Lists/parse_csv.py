c6fcc537647d2b016884e8b09799bc0324c6e123
davidnies <30993499+davidnies@users.noreply.github.com>
2019-07-01 14:57:20 -0500

    Finished video

--- Lists/parse_csv.py
+++ Lists/parse_csv.py
@@ -5,7 +5,7 @@ import csv
 
 with open('names.csv', 'r') as csv_file:   # 'r' = read
     # csv_reader = csv.reader(csv_file)
-    #
+
     #
     # with open('new_names.csv', 'w') as new_file:
     #     csv_writer = csv.writer(new_file, delimiter='\t')
